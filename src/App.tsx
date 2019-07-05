d90d3e463229698494e77a5a4634c74cffa50d90
katerina <katuha_maksimova@mail.ru>
2019-07-05 14:18:08 +0300

    initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    let backdrop: string;
+
+    return (
+     <div> Hello </div>
+    );
+  }
+}
+
+
+
+export { App };
