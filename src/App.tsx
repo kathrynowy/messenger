8930de4018b9e11d8a4e3cf436541761b90b7e14
DmitrySobolevski <Dzmitry.Sabaleuski@itechart-group.com>
2019-07-05 16:45:39 +0300

    add main html

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-
+import './App.scss';
 
 class App extends Component {
   render() {
     let backdrop: string;
 
     return (
-     <div> Hello </div>
+     <div className="app-container">
+       
+     </div>
     );
   }
 }
-
 
 
 export { App };
